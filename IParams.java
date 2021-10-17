866216b67a49f09e843c50cd866c9e1b52ee7bc1
Lin <2339551726@qq.com>
2021-10-17 23:19:19 +0800

    java2

---
+++ IParams.java
@@ -0,0 +1,8 @@
+package gittest;
+import java.util.ArrayList;
+public interface IParams {
+	public int getBig();
+	public int getMedium();
+	public int getSmall();
+	public ArrayList<Integer> getPlanParking();
+}
